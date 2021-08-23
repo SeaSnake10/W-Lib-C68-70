36166aa26ac076a28d9f2b41c7dba26681670edb
SeaSnake10 <76809064+SeaSnake10@users.noreply.github.com>
2021-08-23 17:05:37 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Search from './screens/Search';
 import BT from './screens/BTs';
 import {createAppContainer} from 'react-navigation';
@@ -15,6 +15,23 @@ export default class App extends React.Component{
 
 const TabNavigator = createBottomTabNavigator({
   Transaction:{screen:BT},
-  Search:{screen:Search}
-})
+  Search:{screen:Search},
+},
+{
+  defaultNavigationOptions:({navigation})=> ({
+    tabBarIcon:({})=>{
+      const rootName = navigation.state.routeName
+      if(rootName === "Transaction"){
+        return(
+          <Image source = {require("./assets/book.png")} style={{width:30, height:30}}/>
+        );
+      }else if (rootName === "Search"){
+        return(
+          <Image source = {require("./assets/searchingbook.png")} style={{width:30, height:30}}/>
+        );
+      }
+    }
+  })
+}
+)
 const AppContainer = createAppContainer(TabNavigator)
--- screens/BTs.js
+++ screens/BTs.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import {BarCodeScanner, BarcodeScanner} from 'expo-barcode-scanner';
 
@@ -10,17 +10,25 @@ export default class BT extends React.Component{
             hcp:null,
             scanned:false,
             scannedData:"",
-            buttonState:"normal"
+            buttonState:"normal",
+            scannedBookId:"",
+            scannedStudentId:""
         }
     }
 
-    getCameraPermissions= async ()=>{
+    getCameraPermissions= async (id)=>{
         const {status} = await Permissions.askAsync(Permissions.CAMERA)
-        this.setState({ hcp:status === "granted", buttonState:'clicked' })
+        this.setState({ hcp:status === "granted", buttonState:id, scanned:false});
     }
 
     handleBarCodeScanned= async ({type, data})=>{
-        this.setState({ scanned:true, scannedData:data, buttonState:"normal"})
+        const {buttonState}=this.state
+        if(buttonState === "BookID"){
+            this.setState({ scanned:true, scannedData:data, buttonState:"normal"})
+        }else if (buttonState === "StudentID"){
+            this.setState({ scanned:true, scannedData:data, buttonState:"normal"})
+        }
+
     }
 
     render(){
@@ -28,7 +36,7 @@ export default class BT extends React.Component{
         const scanned = this.state.scanned
         const buttonState = this.state.buttonState
 
-        if(buttonState === "clicked"&& hcp){
+        if(buttonState !== "normal"&& hcp){
             return(
                 <BarCodeScanner 
                 onBarCodeScanned = {scanned ? undefined : this.handleBarCodeScanned }
@@ -39,14 +47,21 @@ export default class BT extends React.Component{
         else if (buttonState === "normal"){
             return(
                 <View style={styles.v}>
-                    <Text>
-                        Returning or Borrowing
-                    </Text>
-                    <TouchableOpacity style={styles.scb} onPress={this.getCameraPermissions}>
+                    <Image source ={require("../assets/booklogo.jpg")} style={{ width:60, height:60}}/>
+                    <Text>Returning or Borrowing</Text>
+                    <TextInput placeholder="Book ID" style={styles.sbid} value={this.state.scannedBookId}/>
+                    <TouchableOpacity style={styles.scb} onPress={this.getCameraPermissions("BookID")}>
                         <Text style={styles.scbText}>
                             Code Scanner
                         </Text>
                     </TouchableOpacity>
+                    <TextInput placeholder="Student ID" style={styles.sbid} value={this.state.scannedStudentId}/>
+                    <TouchableOpacity style={styles.scb} onPress={this.getCameraPermissions("StudentID")}>
+                        <Text style={styles.scbText}>
+                            Student ID Scanner
+                        </Text>
+                    </TouchableOpacity>
+                    
                 </View>
             );
         }
@@ -61,11 +76,21 @@ const styles = StyleSheet.create({
     },
     scb:{
         backgroundColor:'red',
-        width:50,
-        height:50
+        width:150,
+        height:50,
+        borderRadius:50,
+        marginTop:10
     },
     scbText:{
+        marginTop:10,
         fontSize:20,
-        color:'white'
+        color:'white',
+        textAlign:'center'
+    },
+    sbid:{
+        width:200,
+        height:50,
+        borderWidth:2,
+        marginTop:10
     }
 })
